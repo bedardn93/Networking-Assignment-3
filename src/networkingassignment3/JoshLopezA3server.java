0ee8e2d66c10d7925c2f317c5928dc7010628ff6
sinapples <josh.lopez.is@gmail.com>
2015-05-01 00:08:24 -0400

    added t1

--- src/networkingassignment3/JoshLopezA3server.java
+++ src/networkingassignment3/JoshLopezA3server.java
@@ -11,4 +11,69 @@ package networkingassignment3;
  */
 public class JoshLopezA3server {
     
+    
+    
+    
+    
+    //create routing table from WT.txt
+    
+    
+    
+    
+    
+    public static void method (int user_input, int numDVR_messages, int[] message){
+         
+        
+        //time to process 
+        int T1 = 0, T2 = 0;
+                
+                
+        if (user_input == 1) {
+            //reads DVR messages from client 
+            
+            
+            for (int i = 0; i < numDVR_messages; i++) {
+            //Updates the routing table 
+            
+            
+            //prints the routing table 
+                
+                
+                
+            }
+            System.out.println("Elapsed time: " + T1);
+            
+           
+           
+        }
+        
+        else if (user_input == 2) {
+            
+            //thread 1 
+            //reads DVR messages from client 
+            
+            
+            //thread 2
+            //Updates the routing table 
+         
+            //prints the routing table 
+                
+                
+                
+              System.out.println("Elapsed time: " + T2);
+            
+        }
+        else {
+        
+        System.out.println ("invalid input");
+        }
+        
+        
+        
+        
+        
+    }
+    
+    
+    
 }
